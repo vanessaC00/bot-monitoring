995b8f17005e2289b4ea1b23691700b73ffb8920
Vanessa Chan <vanessa.chan@qs.com>
2024-09-27 01:28:06 +0800

    Changes

--- bot_navigation.js
+++ bot_navigation.js
@@ -1,1 +1,14 @@
 /* Used for the functionality of the navigation to the bots */
+//Testing
+window.botpressWebChat.init({
+    "botId": "a4acca45-aa89-4340-996f-e12b79cb2ab9",
+    "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+    "messagingUrl": "https://messaging.botpress.cloud",
+    "clientId": "a4acca45-aa89-4340-996f-e12b79cb2ab9",
+    "webhookId": "4c21b14b-8607-465c-9412-e9e6524fd7de",
+    "frontendVersion": "v1",
+    "useSessionStorage": true,
+    "allowedOrigins": [],
+    userData: userPayload,
+});
+
